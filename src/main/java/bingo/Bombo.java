fed570dc5d062955dd887f4a1f17b303f45f4ed9
maksim <maksimsereda2@gmail.com>
2020-04-12 12:53:56 +0200

    Creamos la clase Bombo con sus getters, setters y tostring

---
+++ src/main/java/bingo/Bombo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bingo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Maksim
+ */
+public class Bombo {
+    ArrayList<Integer> bombo;
+
+    public Bombo() {
+        this.bombo  = new ArrayList<Integer>();//El bombo sera una array list de la que iremos sacando los numeros.
+        for (int i = 1; i < 91; i++) {
+            this.bombo.add(i);
+        }
+    }
+
+    public ArrayList<Integer> getBombo() {
+        return bombo;
+    }
+
+    public void setBombo(ArrayList<Integer> bombo) {
+        this.bombo = bombo;
+    }
+
+    @Override
+    public String toString() {
+        return "Bombo{" + "bombo=" + bombo + '}';
+    }
+    
+}
