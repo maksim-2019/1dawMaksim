06d596cb877af36b13d5f0575d4e35be6d5920c2
maksim <maksimsereda2@gmail.com>
2020-04-12 14:23:27 +0200

    Añadimos Los atributos de carton y los metodos basicos. getters, setters, toString

--- src/main/java/bingo/Carton.java
+++ src/main/java/bingo/Carton.java
@@ -10,5 +10,42 @@ package bingo;
  * @author Maksim
  */
 public class Carton {
+
+    private int fila1[] = new int[9];
+    private int fila2[] = new int[9];
+    private int fila3[] = new int[9];
+
+    public Carton() {
+    }
+
+    public int[] getFila1() {
+        return fila1;
+    }
+
+    public void setFila1(int[] fila1) {
+        this.fila1 = fila1;
+    }
+
+    public int[] getFila2() {
+        return fila2;
+    }
+
+    public void setFila2(int[] fila2) {
+        this.fila2 = fila2;
+    }
+
+    public int[] getFila3() {
+        return fila3;
+    }
+
+    public void setFila3(int[] fila3) {
+        this.fila3 = fila3;
+    }
+
+    @Override
+    public String toString() {
+        return "Carton{" + "fila1=" + fila1 + ", fila2=" + fila2 + ", fila3=" + fila3 + '}';
+    }
+    
     
 }
