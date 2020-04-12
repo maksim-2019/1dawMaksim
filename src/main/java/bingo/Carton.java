ddf30ddb0d436f579a74f501a50ceb1e354940f4
maksim <maksimsereda2@gmail.com>
2020-04-12 16:38:38 +0200

    Creando metodo generarCarton y empezando algoritmo de generacion de numeros

--- src/main/java/bingo/Carton.java
+++ src/main/java/bingo/Carton.java
@@ -5,47 +5,71 @@
  */
 package bingo;
 
+import java.util.Random;
+
 /**
  *
  * @author Maksim
  */
 public class Carton {
 
-    private int fila1[] = new int[9];
-    private int fila2[] = new int[9];
-    private int fila3[] = new int[9];
+    private int matriz[][] = new int[3][9];
 
     public Carton() {
     }
 
-    public int[] getFila1() {
-        return fila1;
+    public void generarCarton() {
+        Random alea = new Random();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (j == 0) {
+                    if (i > 0) {
+                        matriz[0][0] = 0;
+//                        if (matriz[i - 1][j] == 9 || matriz[i - 1][j] == 0) {
+//                            matriz[i][j] = 0;
+//                        } else 
+                        if (i == 1 && matriz[i - 1][0] == 0) {
+                            matriz[i][j] = alea.nextInt(8) + 1;
+                        } else if (i == 2 && matriz[i - 1][0] > 1) {
+                            matriz[i][j] = 0;
+                        } else {
+                            int num = matriz[i - 1][j] + 1;
+                            matriz[i][j] = alea.nextInt(9 - num + 1) + num;
+                        }
+                    } else {
+                        matriz[i][j] = alea.nextInt(8);
+                        System.out.println("------------");
+                    }
+
+                }
+                //matriz[i][j] = alea.nextInt(90) + 10;
+            }
+        }
+
     }
 
-    public void setFila1(int[] fila1) {
-        this.fila1 = fila1;
+    public int[][] getMatriz() {
+        return matriz;
     }
 
-    public int[] getFila2() {
-        return fila2;
-    }
-
-    public void setFila2(int[] fila2) {
-        this.fila2 = fila2;
-    }
-
-    public int[] getFila3() {
-        return fila3;
-    }
-
-    public void setFila3(int[] fila3) {
-        this.fila3 = fila3;
+    public void setMatriz(int[][] matriz) {
+        this.matriz = matriz;
     }
 
     @Override
     public String toString() {
-        return "Carton{" + "fila1=" + fila1 + ", fila2=" + fila2 + ", fila3=" + fila3 + '}';
+        return "Carton{" + "matriz=" + matriz + '}';
+
     }
-    
-    
+
+    public static void main(String[] args) {
+        Carton ale = new Carton();
+        ale.generarCarton();
+        for (int i = 0; i < ale.getMatriz().length; i++) {
+            for (int j = 0; j < ale.getMatriz()[i].length; j++) {
+                System.out.print(ale.getMatriz()[i][j]);
+            }
+            System.out.println("\n--------------");
+        }
+    }
 }
