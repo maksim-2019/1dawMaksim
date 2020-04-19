3e600923f464087e0e76f7b69e0ed8b16a1c28d1
maksim <maksimsereda2@gmail.com>
2020-04-19 18:16:17 +0200

    Cambio de metodo generarCarton y nuevo metodo ponerCero

--- src/main/java/bingo/Carton.java
+++ src/main/java/bingo/Carton.java
@@ -23,125 +23,170 @@ public class Carton {
         //Aqui pasare a explicar el funcionamiento del algoritmo.
         Random alea = new Random();//Este random se usa para generar numeros aleatorios que se usan el el algoritmo.
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {//Esto es un doble for que recore la matriz.
-                if (j == 0) {//El algoritmo estara dividido en columnas con los if controlare que se rellene cada columna con sus numeros corespondientes.
-                    if (i >= 0) {//Inicio columna 1:
-                        if (i == 0 && j == 0) {//En la primera posicion de la primera colunna.
-                            int num2 = alea.nextInt(3);//se decide con una con una provavilidad entre 2 de que salga un numero o este vacio
-                            if (num2 == 0) { //Que la casilla este vacia se representa con un cero.
-                                matriz[i][j] = 0;
-                            } else {//Con este if , else se pone un 0 o un numero aleatorio entre 1-9 
-                                matriz[i][j] = alea.nextInt(8) + 1;
-                            }
-                        } else if (i == 1 && matriz[i - 1][0] == 0) {//Este else if comprueba si la posicion anterior no tiene un numero asigrado
-                            int num2 = alea.nextInt(3);//Si se cumple la condicion se vuelve a generar un numero aleatorio y con 
-                            //este se decide si este numero esta vacio o contienen un numero entre el 1-9
-                            if (num2 == 0) {
-                                matriz[i][j] = 0;
+            for (int j = 0; j < matriz[i].length; j++) {
+                switch (j) {
+                    case 0:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(8) + 1;
+                        } else if (i == 1 && matriz[i - 1][j] < 9) {
+                            matriz[i][j] = alea.nextInt(9 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 9) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 9) {
+                                matriz[i][j] = alea.nextInt(9 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
                             } else {
-                                matriz[i][j] = alea.nextInt(8) + 1;
+                                matriz[i][j] = alea.nextInt(9 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
                             }
-                        } else if (i == 2 && matriz[i - 1][0] > 1 && matriz[i - 2][0] > 1) {
-                            //Este else if comprueba si las anteriores posiciones tienen un numero,
-                            //si lo tienen pondra un cero para que no haya tres numeros seguidos. 
-                            matriz[i][j] = 0;
-                        } else {//Este else se ejecuta si ninguna de las anteriores opciones se cumple.
-                            int num;
-                            if (matriz[0][0] == 9) {
-                                matriz[1][0] = 0;
-                                matriz[2][0] = 0;
-                            } else if (matriz[1][0] == 9) {
-                                matriz[2][0] = 0;
-                            } else {
-                                if (matriz[0][0] > 1 && matriz[1][0] == 0) {
-                                    num = matriz[0][0] + 1;
-                                } else {
-                                    num = matriz[i - 1][j] + 1;
-                                }
-                                matriz[i][j] = alea.nextInt(9 - num + 1) + num;
-                            }
-
-                        }
-                    }
-                    //Fin columna 1
-                } else if (j == 1) {//Principio columna 2:
-                    if (i >= 0) {
-                        if (i == 0 && j == 1) {
-                            int num2 = alea.nextInt(2);
-                            if (num2 == 0) {
-                                matriz[i][j] = 0;
-                            } else {
-                                matriz[i][j] = alea.nextInt(19 - 10 + 1) + 10;
-                            }
-                        } else if (i == 1 && matriz[i - 1][1] == 0) {
-                            int num2 = alea.nextInt(2);
-                            if (num2 == 0) {
-                                matriz[i][j] = 0;
-                            } else {
-                                matriz[i][j] = alea.nextInt(19 - 10 + 1) + 10;
-                            }
-                        } else if (i == 2 && matriz[i - 1][1] > 1 && matriz[i - 2][1] > 1) {
-                            matriz[i][j] = 0;
                         } else {
-                            int num;
-                            if (matriz[0][1] == 19) {
-                                matriz[1][1] = 0;
-                                matriz[2][1] = 0;
-                            } else if (matriz[1][1] == 19) {
-                                matriz[2][1] = 0;
-                            } else {
-                                if (matriz[0][1] > 1 && matriz[1][1] == 0) {
-                                    num = matriz[0][1] + 1;
-                                } else {
-                                    num = matriz[i - 1][j] + 1;
-                                }
-                                matriz[i][j] = alea.nextInt(19 - num + 1) + num;
-                            }
-
-                        }
-                    }
-                    //Fin columna 2
-                } else if (j == 2) {//Principio columna 3
-                    if (i >= 0) {
-                        if (i == 0 && j == 2) {
-                            int num2 = alea.nextInt(2);
-                            if (num2 == 0) {
-                                matriz[i][j] = 0;
-                            } else {
-                                matriz[i][j] = alea.nextInt(29 - 20 + 1) + 20;
-                            }
-                        } else if (i == 1 && matriz[i - 1][2] == 0) {
-                            int num2 = alea.nextInt(2);
-                            if (num2 == 0) {
-                                matriz[i][j] = 0;
-                            } else {
-                                matriz[i][j] = alea.nextInt(29 - 20 + 1) + 20;
-                            }
-                        } else if (i == 2 && matriz[i - 1][2] > 1 && matriz[i - 2][2] > 1) {
                             matriz[i][j] = 0;
-                        } else {
-                            int num;
-                            if (matriz[0][2] == 19) {
-                                matriz[1][2] = 0;
-                                matriz[2][2] = 0;
-                            } else if (matriz[1][2] == 19) {
-                                matriz[2][2] = 0;
-                            } else {
-                                if (matriz[0][2] > 1 && matriz[1][2] == 0) {
-                                    num = matriz[0][2] + 1;
-                                } else {
-                                    num = matriz[i - 1][j] + 1;
-                                }
-                                matriz[i][j] = alea.nextInt(29 - num + 1) + num;
-                            }
                         }
-                    }
-                    //Fin columna 3    
+                        break;
+                    case 1:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(19 - 10 + 1) + 10;
+                        } else if (i == 1 && matriz[i - 1][j] < 19) {
+                            matriz[i][j] = alea.nextInt(19 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 19) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 19) {
+                                matriz[i][j] = alea.nextInt(19 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(19 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(19 - 10 + 1) + 10;
+                        break;
+                    case 2:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(29 - 20 + 1) + 20;
+                        } else if (i == 1 && matriz[i - 1][j] < 29) {
+                            matriz[i][j] = alea.nextInt(29 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 29) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 29) {
+                                matriz[i][j] = alea.nextInt(29 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(29 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(29 - 20 + 1) + 20;
+                        break;
+                    case 3:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(39 - 30 + 1) + 30;
+                        } else if (i == 1 && matriz[i - 1][j] < 39) {
+                            matriz[i][j] = alea.nextInt(39 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 39) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 39) {
+                                matriz[i][j] = alea.nextInt(39 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(39 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(39 - 30 + 1) + 30;
+                        break;
+                    case 4:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(49 - 40 + 1) + 40;
+                        } else if (i == 1 && matriz[i - 1][j] < 49) {
+                            matriz[i][j] = alea.nextInt(49 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 49) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 49) {
+                                matriz[i][j] = alea.nextInt(49 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(49 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(49 - 40 + 1) + 40;
+                        break;
+                    case 5:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(59 - 50 + 1) + 50;
+                        } else if (i == 1 && matriz[i - 1][j] < 59) {
+                            matriz[i][j] = alea.nextInt(59 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 59) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 59) {
+                                matriz[i][j] = alea.nextInt(59 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(59 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(59 - 50 + 1) + 50;
+                        break;
+                    case 6:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(69 - 60 + 1) + 60;
+                        } else if (i == 1 && matriz[i - 1][j] < 19) {
+                            matriz[i][j] = alea.nextInt(69 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 69) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 69) {
+                                matriz[i][j] = alea.nextInt(69 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(69 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(69 - 60 + 1) + 60;
+                        break;
+                    case 7:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(79 - 70 + 1) + 70;
+                        } else if (i == 1 && matriz[i - 1][j] < 79) {
+                            matriz[i][j] = alea.nextInt(79 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 79) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 79) {
+                                matriz[i][j] = alea.nextInt(79 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(79 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(79 - 70 + 1) + 70;
+                        break;
+                    case 8:
+                        if (i == 0) {
+                            matriz[i][j] = alea.nextInt(90 - 80 + 1) + 80;
+                        } else if (i == 1 && matriz[i - 1][j] < 90) {
+                            matriz[i][j] = alea.nextInt(90 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                        } else if (i == 2 && matriz[i - 1][j] < 90) {
+                            if (matriz[i - 1][j] == 0 && matriz[i - 2][j] < 90) {
+                                matriz[i][j] = alea.nextInt(90 - matriz[i - 2][j] + 1) + matriz[i - 2][j];
+                            } else {
+                                matriz[i][j] = alea.nextInt(90 - matriz[i - 1][j] + 1) + matriz[i - 1][j];
+                            }
+                        } else {
+                            matriz[i][j] = 0;
+                        }
+                        //matriz[i][j] = alea.nextInt(90 - 80 + 1) + 80;
+                        break;
                 }
-
-                //matriz[i][j] = alea.nextInt(90) + 10;
             }
         }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (i == 1) {
+                    if (matriz[i][j] == matriz[i - 1][j]) {
+                        matriz[i][j] = 0;
+                    }
+                } else if (i == 2) {
+                    if (matriz[i][j] == matriz[i - 1][j] || matriz[i][j] == matriz[i - 2][j]) {
+                        matriz[i][j] = 0;
+                    }
+                }
+
+            }
+        }
+        ponerCero(matriz);
 
     }
 
@@ -159,12 +204,42 @@ public class Carton {
 
     }
 
+    private static void ponerCero(int aux[][]) {
+        int contador = 0;
+        int fila1 = 0;
+        int fila2 = 0;
+        int fila3 = 0;
+        Random alea = new Random();
+        int num;
+        for (int i = 0; i < 9; i++) {
+            if (aux[0][i] >= 1 && aux[1][i] >= 1 && aux[2][i] >= 1) {
+                num = alea.nextInt(3);
+                aux[num][i] = 0;
+            }
+        }
+        for (int i = 0; i < aux.length; i++) {
+            for (int j = 0; j < aux[i].length; j++) {
+                if (i == 0 && aux[i][j] == 0) {
+                    fila1++;
+                } else if (i == 1 && aux[i][j] == 0) {
+                    fila2++;
+                } else if (i == 2 && aux[i][j] == 0) {
+                    fila3++;
+                }
+            }
+        }
+
+           System.out.println(fila1);
+           System.out.println(fila2);
+           System.out.println(fila3);
+    }
+
     public static void main(String[] args) {
         Carton ale = new Carton();
         ale.generarCarton();
         for (int i = 0; i < ale.getMatriz().length; i++) {
             for (int j = 0; j < ale.getMatriz()[i].length; j++) {
-                System.out.print(ale.getMatriz()[i][j] + " | ");
+                System.out.print(ale.getMatriz()[i][j] + "\t|\t");
             }
             System.out.println("\n-----------------------------------------------");
         }
